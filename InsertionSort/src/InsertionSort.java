2398c7ccef8a00e0f8094d845d4eb1c7375e41ea
Hartanto Andreas <handreas@paypal.com>
2016-03-08 10:51:51 +0800

    fix

---
+++ InsertionSort/src/InsertionSort.java
@@ -0,0 +1,10 @@
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+
+		if(args.length != 1) {
+			throw new IllegalArgumentException("expecting one argument");
+		}
+	}
+}
